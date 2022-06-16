70fe32195111df78e8831a4ca5c6778d8d376442
korsdrag <Gabriel@tengens.se>
2022-06-16 12:27:57 +0200

    Todo app complete

--- grupp5_react_projekt/src/App.js
+++ grupp5_react_projekt/src/App.js
@@ -1,14 +1,58 @@
-import './App.css';
-import React from 'react';
-import TodoList from './components/TodoList';
+import "./App.css";
+import React from "react";
+import TodoList from "./components/TodoList";
+import { useState, useRef, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
 
+
+const LOCAL_STORAGE_KEY = "todoApp.todos";
 function App() {
+  const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+  const [todos, setTodos] = useState(storedTodos);
+  const todoNameRef = useRef();
+  
+  //används om react.strictmode inte används
+  // useEffect(() => {
+  //   const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+  //   if (storedTodos) setTodos(storedTodos)
+  // },[])
+
+
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
+
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(x => x.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value;
+    if (name === "") {
+      return;
+    }
+    setTodos((x) => {
+      return [...x, { id: uuidv4(), name: name, complete: false }];
+    });
+    todoNameRef.current.value = null;
+  }
+  function clearTodo() {
+    const newTodos = todos.filter(x => !x.complete)
+    setTodos(newTodos);
+  }
+  
+  console.log(todos);
   return (
     <div className="App">
-
-      <TodoList />
-      <input type="text"></input>
-      <button>Add todo</button>
+      <TodoList todoList={todos} toggleTodo={toggleTodo} />
+      <input type="text" ref={todoNameRef}></input>
+      <button onClick={handleAddTodo}>Add todo</button>
+      <button onClick={clearTodo}>Clear todo</button>
+      <p>{todos.filter(x => !x.complete).length} left todo</p>
     </div>
   );
 }
--- grupp5_react_projekt/src/components/TodoList.js
+++ grupp5_react_projekt/src/components/TodoList.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import Todo from './Todo'
 
-export default function TodoList() {
+export default function TodoList( { todoList, toggleTodo } ) {
   return (
-    <h1>TodoList</h1>
-  )
+    todoList.map( todo => { 
+        return <Todo key={todo.id} todo={todo} toggleTodo={toggleTodo}/>
+    } ) 
+  ) 
 }
