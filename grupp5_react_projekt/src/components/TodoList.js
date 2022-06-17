281dc425bdf30a31ef81aea19817daa5501d7135
Oscar Sönnerborg <sonner_borg@hotmail.com>
2022-06-17 09:13:05 +0200

    sista

--- grupp5_react_projekt/src/components/TodoList.js
+++ grupp5_react_projekt/src/components/TodoList.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import Todo from './ToDo'
 
-export default function TodoList({todos}) {
+import React from 'react'
+import Todo from './Todo'
+
+export default function TodoList( { todoList, toggleTodo } ) {
   return (
-    todos.map(todo => {
-      return <Todo key = {todo.id} todo = {todo}/>
-    })
-  )
+    todoList.map( todoItem => { 
+        return <Todo key={todoItem.id} todoItem={todoItem} toggleTodo={toggleTodo}/>
+    } ) 
+  ) 
 }
- 
